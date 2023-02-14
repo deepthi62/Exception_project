329aca96d18a290389899065bd6965bdbea73907
deepthi62 <121333217+deepthi62@users.noreply.github.com>
2023-02-14 10:58:49 +0530

    file uploaded

---
+++ Exceptionproject/src/ExcehandlingThrow/Ex1.java
@@ -0,0 +1,40 @@
+package ExcehandlingThrow;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		String name = "70";
+		int n1 = 20;
+		int n2 = 0;
+		int[]namearray = new int[5];
+		try {
+		
+		int namestring1 = Integer.parseInt(name);
+		System.out.println(namestring1);
+		try {
+		
+		int n3 = n1/n2;
+		System.out.println(n3);
+		try {
+			System.out.println(namearray[6]);
+		}
+		catch(ArrayIndexOutOfBoundsException ary){
+			System.out.println(ary);
+	}
+     	}
+		catch(NumberFormatException ae) {
+			System.out.println(ae);
+		}
+    }
+		catch(ArithmeticException e) {
+			System.out.println(e);
+		
+		}catch(Exception e) {
+			System.out.println("--------");
+		  } 
+	}
+}
+
+	
+
+
